2be87cdda95f3fffc0d3ca979e5e854e74debf06
Jonathan Shepard <s1482817@student.mcckc.edu>
2019-12-09 20:07:34 -0600

    Final Commit

---
+++ jsCalendar-master/source/todo.js
@@ -0,0 +1,61 @@
+document.write("<b><center>See! That wasn't so hard!</center></b>");
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Adds a checkmark when clicked
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Adds a new list item when "Add" is clicked on
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  try{
+  if (inputValue === '') {
+    throw(ex = "You must write something you dirty cheater!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  }catch(ex){
+  alert(ex);}
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  
+
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
